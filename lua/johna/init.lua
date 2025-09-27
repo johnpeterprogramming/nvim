4f17af9a6e02aa7be14c9e468a7d72b159cbe866
johnpeterprogramming <u23533529@tuks.co.za>
2025-09-27 11:56:52 +0200

    some changes... amend later.

--- lua/johna/init.lua
+++ lua/johna/init.lua
@@ -2,4 +2,6 @@ vim.g.mapleader = " "
 
 require("johna.lazy_init")
 require("johna.remap")
+print("remap loaded")
 require("johna.set")
+print("defaults loaded")
--- lua/johna/remap.lua
+++ lua/johna/remap.lua
@@ -1,22 +1,27 @@
 local which_key = require("which-key")
-local non_lsp_mappings = {
-    ["<leader>"] = {
-        ["2O"] = { "O<Esc>O", "Write two new lines above and go into insert mode." },
-        ["pf"] = { "<cmd>lua require('telescope.builtin').find_files()<CR>", "Find Files" },
-        ["ps"] = { "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })<CR>", "Grep String" },
-        e = { vim.cmd.Ex, "Open file explorer" },
+which_key.add({
+    { "2O", "O<Esc>O", desc = "Write two new lines above and go into insert mode." },
+    { "<leader>pv", vim.cmd.Ex, desc = "Open file explorer" },
+    { "<leader>ff", "<cmd>lua require('telescope.builtin').find_files()<CR>", desc = "Find Files" },
+    { "<leader>fg", "<cmd>lua require('telescope.builtin').grep_string({ search = vim.fn.input('Grep > ') })<CR>", desc = "Grep String" },
+    { "<leader>e",
+        function()
+        vim.diagnostic.open_float(nil, { focusable = false })
+        end,
+        desc = "Show Diagnostic",
+    },
+})
+
+--  I can add these later when I understand what they do
+--
     --    p = { "\"_dP", "Paste without overwrite" },
     --    ["/"] = { "<Plug>(comment_toggle_linewise_current)", "Toggle comment" },
     --    s = { [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], "Search and replace word under cursor" },
     --    t = { ":Today<CR>", "Open today's note" },
-    },
-    ["<C-p>"] = { "<cmd>lua require('telescope.builtin').git_files()<CR>", "Git Files" },
 --  J = { "mzJ`z", "Join lines and keep cursor position" },
 --  ["<C-d>"] = { "<C-d>zz", "Half page down and center" },
 --  ["<C-u>"] = { "<C-u>zz", "Half page up and center" },
 --  n = { "nzzzv", "Next search result and center" },
 --  N = { "Nzzzv", "Previous search result and center" },
 --  Q = { "<nop>", "Disable Ex mode" },
-}
- 
-which_key.register(non_lsp_mappings)
+
